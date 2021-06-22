226455403bb1a621879b64cfbcd06e51fc3f9265
cd4d <11580810+cd4d@users.noreply.github.com>
2021-06-21 20:43:28 -0400

    Local env, removed API key from environments

---
+++ scripts/setenv.ts
@@ -0,0 +1,25 @@
+// https://javascript.plainenglish.io/setup-dotenv-to-access-environment-variables-in-angular-9-f06c6ffb86c0
+
+const { writeFile } = require('fs');
+const { argv } = require('yargs'); // read environment variables from .env file
+require('dotenv').config(); // read the command line arguments passed with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+const targetPath = isProduction
+  ? `./src/environments/environment.prod.ts`
+  : `./src/environments/environment.ts`; // we have access to our environment variables
+// in the process.env object thanks to dotenv
+const environmentFileContent = `
+export const environment = {
+   production: ${isProduction},
+   rapidAPIKey: "${process.env.rapidAPIKey}",
+  
+};
+`;
+// write the content to the respective file
+writeFile(targetPath, environmentFileContent, function (err:any) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Wrote variables to ${targetPath}`);
+});
